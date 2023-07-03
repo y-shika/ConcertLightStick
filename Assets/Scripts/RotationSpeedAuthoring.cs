46af362cd1f131091922fd2d2ccc55cfbe301563
y-shika <shikanai327327@gmail.com>
2023-07-03 19:23:29 +0900

    add: first sample of ECS

---
+++ Assets/Scripts/RotationSpeedAuthoring.cs
@@ -0,0 +1,25 @@
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class RotationSpeedAuthoring : MonoBehaviour
+{
+    [SerializeField] private float degreesPerSecond = 360f;
+    
+    class Baker : Baker<RotationSpeedAuthoring>
+    {
+        public override void Bake(RotationSpeedAuthoring authoring)
+        {
+            var entity = GetEntity(TransformUsageFlags.Dynamic);
+            AddComponent(entity, new RotationSpeed
+            {
+                RadiansPerSecond = math.radians(authoring.degreesPerSecond)
+            });
+        }
+    }
+}
+
+struct RotationSpeed : IComponentData
+{
+    public float RadiansPerSecond;
+}
