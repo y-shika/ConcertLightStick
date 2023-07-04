5d6fc267593f526ba63cdcd88025296b14b53903
y-shika <shikanai327327@gmail.com>
2023-07-04 23:11:00 +0900

    add: aspect sample

---
+++ Assets/Scripts/VerticalMovementAspect.cs
@@ -0,0 +1,14 @@
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+readonly partial struct VerticalMovementAspect : IAspect
+{
+    readonly RefRW<LocalTransform> m_Transform;
+    readonly RefRO<RotationSpeed> m_Speed;
+
+    public void Move(double elapsedTime)
+    {
+        m_Transform.ValueRW.Position.y = (float) math.sin(elapsedTime * m_Speed.ValueRO.RadiansPerSecond);
+    }
+}
