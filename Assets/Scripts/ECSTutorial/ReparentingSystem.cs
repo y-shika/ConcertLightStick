0c444bd928e9416170444f8994789f8872634dff
y-shika <shikanai327327@gmail.com>
2023-07-07 18:32:05 +0900

    add: reparenting sample

---
+++ Assets/Scripts/ECSTutorial/ReparentingSystem.cs
@@ -0,0 +1,56 @@
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Transforms;
+
+namespace ECSTutorial
+{
+    public partial struct ReparentingSystem : ISystem
+    {
+        private float _time;
+        private const float Interval = 0.7f;
+        private bool _attached;
+        
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+            _time = Interval;
+            _attached = true;
+        }
+        
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+            _time -= SystemAPI.Time.DeltaTime;
+            if (_time > 0)
+            {
+                return;
+            }
+
+            _time = Interval;
+
+            var rotatorEntity = SystemAPI.GetSingletonEntity<RotationSpeed>();
+            var ecb = new EntityCommandBuffer(Allocator.Temp);
+
+            if (_attached)
+            {
+                var children = SystemAPI.GetBuffer<Child>(rotatorEntity);
+
+                for (int i = 0; i < children.Length; i++)
+                {
+                    ecb.RemoveComponent<Parent>(children[i].Value);
+                }
+            }
+            else
+            {
+                foreach (var (transform, entity) in SystemAPI.Query<RefRO<LocalTransform>>().WithNone<RotationSpeed>().WithEntityAccess())
+                {
+                    ecb.AddComponent(entity, new Parent { Value = rotatorEntity });
+                }
+            }
+            
+            ecb.Playback(state.EntityManager);
+            _attached = !_attached;
+        }
+    }
+}
